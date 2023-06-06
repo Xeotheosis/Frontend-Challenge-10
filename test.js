b2389e41712b449e10e67d2d0b685b2c29fa8ddf
Andrei <126566419+Xeotheosis@users.noreply.github.com>
2023-06-06 21:15:45 +0300

    Update test.js

--- test.js
+++ test.js
@@ -13,79 +13,73 @@ button.addEventListener("click", function() {
     errorDay.style.display = "block";
   } else {
     errorDay.style.display = "none";
+    if (dayInput.value <= 0 || dayInput.value > 31) {
+      errorDay.innerHTML = "Must be a valid day";
+      errorDay.style.display = "block";
+    }
   }
+
   if (monthInput.value.trim() === "") {
     errorMonth.style.display = "block";
   } else {
     errorMonth.style.display = "none";
+    if (monthInput.value <= 0 || monthInput.value > 12) {
+      errorMonth.innerHTML = "Must be a valid month";
+      errorMonth.style.display = "block";
+    }
   }
+
   if (yearInput.value.trim() === "") {
     errorYear.style.display = "block";
   } else {
     errorYear.style.display = "none";
+    if (yearInput.value <= 0 || yearInput.value > 2023) {
+      errorYear.innerHTML = "Must be in the past";
+      errorYear.style.display = "block";
+    }
   }
 
-  if (dayInput.value <= 0 || dayInput.value > 31) {
-    errorDay.innerHTML = "Must be a valid day";
-    errorDay.style.display = "block";
-  } else {
-    errorDay.style.display = "none";
-  }
-  if (monthInput.value <= 0 || monthInput.value > 12) {
-    errorMonth.innerHTML = "Must be a valid month";
-    errorMonth.style.display = "block";
-  } else {
-    errorMonth.style.display = "none";
-  }
-  if (yearInput.value <= 0 || yearInput.value > 2023) {
-    errorYear.innerHTML = "Must be in the past";
-    errorYear.style.display = "block";
-  } else {
-    errorYear.style.display = "none";
-  }
-
-  // Run calculateAge() only if all input fields are non-empty
+  // Run calculateAge() only if all input fields are non-empty and have valid values
   if (
     dayInput.value.trim() !== "" &&
     monthInput.value.trim() !== "" &&
     yearInput.value.trim() !== "" &&
-    yearInput.value <= 2023 &&
-    monthInput.value <= 12 &&
+    yearInput.value < 2023 &&
+    monthInput.value < 12 &&
     dayInput.value < 31
   ) {
     calculateAge();
   }
-  
-  
 });
 
 function calculateAge() {
-  let fullDate = new Date(yearInput.value, monthInput.value - 1, dayInput.value);
-  let currentDate = new Date();
-  let years = currentDate.getFullYear() - fullDate.getFullYear();
-  let months = currentDate.getMonth() - fullDate.getMonth();
-  let days = currentDate.getDate() - fullDate.getDate();
-
-  if (months < 0 || (months === 0 && days < 0)) {
-    years--;
-    if (months < 0) {
-      months += 12;
-    }
+    let fullDate = new Date(yearInput.value, monthInput.value - 1, dayInput.value);
+    let currentDate = new Date();
+    let years = currentDate.getFullYear() - fullDate.getFullYear();
+    let months = currentDate.getMonth() - fullDate.getMonth();
+    let days = currentDate.getDate() - fullDate.getDate();
+  
     if (days < 0) {
       let prevMonthLastDay = new Date(
         currentDate.getFullYear(),
-        currentDate.getMonth(),
+        currentDate.getMonth() - 1,
         0
       ).getDate();
       days += prevMonthLastDay;
+      months--;
     }
+  
+    if (months < 0) {
+      months += 12;
+      years--;
+    }
+  
+    let resultsDay = document.getElementById("result-days");
+    let resultsMonth = document.getElementById("result-months");
+    let resultsYear = document.getElementById("result-years");
+  
+    resultsDay.innerHTML = days;
+    resultsMonth.innerHTML = months;
+    resultsYear.innerHTML = years;
   }
-
-  let resultsDay = document.getElementById("result-days");
-  let resultsMonth = document.getElementById("result-months");
-  let resultsYear = document.getElementById("result-years");
-
-  resultsDay.innerHTML = days;
-  resultsMonth.innerHTML = months;
-  resultsYear.innerHTML = years;
-}
+  
